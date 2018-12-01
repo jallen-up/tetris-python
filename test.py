25af819d59656e9828366b5b8df8b8fd0f49811e
John Allen <johnallen3991@gmail.com>
2018-12-01 16:37:22 -0600

    Add block sprite

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
 import pygame
+import time
 
+class GameState:
+    ypos = 0
+    def tick(self):
+        #ypos += 1
+        print("ticking")
+            
 def main():
+    state = GameState()
+
     pygame.init()
 
     screen = pygame.display.set_mode((240, 180))
 
-    image = pygame.image.load("/home/jallen/Pictures/test.png")
+    image = pygame.image.load("block.png")
     screen.blit(image, (50, 50))
     pygame.display.flip()
 
     running = True
     while running:
+        state.tick()
+        time.sleep(1)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
