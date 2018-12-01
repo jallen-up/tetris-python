77009ccc9a6d7f743d4ad47331f408eaadaf23d3
John Allen <johnallen3991@gmail.com>
2018-12-01 17:43:38 -0600

    Move piece right and left

--- test.py
+++ test.py
@@ -1,31 +1,62 @@
 import pygame
 import time
 
+#class Piece:
+#    def __init__(self):
+
 class GameState:
-    ypos = 0
+    def __init__(self):
+        self.running = True
+        self.xpos = 50
+        self.ypos = 50
+        self.screen = pygame.display.set_mode((240, 180))
+        self.tick_count = 0
+        self.tick_mod = 100
+
     def tick(self):
-        #ypos += 1
-        print("ticking")
-            
-def main():
-    state = GameState()
-
-    pygame.init()
-
-    screen = pygame.display.set_mode((240, 180))
-
-    image = pygame.image.load("block.png")
-    screen.blit(image, (50, 50))
-    pygame.display.flip()
-
-    running = True
-    while running:
-        state.tick()
-        time.sleep(1)
-
+        self.tick_count += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.handle_keydown(event.key)
+            elif event.type == pygame.KEYUP:
+                self.handle_keyup(event.key)
+
+        if (self.tick_count % self.tick_mod == 0):
+            self.ypos += 16
+            self.tick_count = 0
+
+        self.draw_screen()
+
+    def handle_keydown(self, key):
+        if key == pygame.K_RIGHT:
+            self.xpos += 16
+        elif key == pygame.K_LEFT:
+            self.xpos -= 16
+
+    def handle_keyup(self, key):
+        print("UP")
+
+    def draw_background(self):
+        self.screen.fill((0, 0, 0))
+
+    def draw_screen(self):
+        self.draw_background()
+        image = pygame.image.load("block.png")
+        self.screen.blit(image, (self.xpos, self.ypos))
+        pygame.display.flip()
+
+            
+def main():
+    pygame.init()
+    state = GameState()
+
+    running = True
+    while state.running:
+        state.tick()
+        time.sleep(0.01)
+
 
 if __name__=="__main__":
     main()
