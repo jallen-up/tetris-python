a5f8bdb4a2436ec82c37fc10f20e1bb855751756
John Allen <johnallen3991@gmail.com>
2018-12-08 11:35:59 -0600

    Semi working wallkicks, broken left/right movement

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import time
 from PIL import Image
 
 block = pygame.image.load("block.png")
-screen = pygame.display.set_mode((160, 320))
+screen = pygame.display.set_mode((600, 480))
 
 wallkick_offsets = [[(0, 0), (-1, 0), (-1, 1), (0, -2), (-1, -2)],
                     [(0, 0), (1, 0), (1, -1), (0, 2), (1, 2)],
@@ -19,13 +19,13 @@ wallkick_offsets_i = [[(0, 0), (-2, 0), (1, 0), (-2, -1), (1, 2)],
 
 # TODO: Rewrite how pieces are identified
 class Piece:
-    LINE_PIECE    = 0
-    SQUARE_PIECE  = 1
+    I_PIECE       = 0
+    O_PIECE       = 1
     LEFT_L_PIECE  = 2
     RIGHT_L_PIECE = 3
     T_PIECE       = 4
-    LEFT_Z_PIECE  = 5
-    RIGHT_Z_PIECE = 6
+    LEFT_J_PIECE  = 5
+    RIGHT_J_PIECE = 6
 
     def __init__(self, state):
         self.shape = random.randint(0, 6)
@@ -34,30 +34,33 @@ class Piece:
         self.y = 0
         self.game_state = state
 
-        if (self.shape == self.LINE_PIECE):
-            self.rotations = Image.open('line.png').load()
+        if (self.shape == self.I_PIECE):
+            im = Image.open('i.png')
             self.color = "Blue"
-        elif (self.shape == self.SQUARE_PIECE):
-            self.rotations = Image.open('square.png').load()
+        elif (self.shape == self.O_PIECE):
+            im = Image.open('o.png')
             self.color = "Yellow"
         elif (self.shape == self.LEFT_L_PIECE):
-            self.rotations = Image.open('left_l.png').load()
+            im = Image.open('left_l.png')
             self.color = "Orange"
         elif (self.shape == self.RIGHT_L_PIECE):
-            self.rotations = Image.open('right_l.png').load()
+            im = Image.open('right_l.png')
             self.color = "Cyan"
         elif (self.shape == self.T_PIECE):
-            self.rotations = Image.open('t.png').load()
+            im = Image.open('t.png')
             self.color = "Purple"
-        elif (self.shape == self.LEFT_Z_PIECE):
-            self.rotations = Image.open('left_z.png').load()
+        elif (self.shape == self.LEFT_J_PIECE):
+            im = Image.open('left_j.png')
             self.color = "Green"
-        elif (self.shape == self.RIGHT_Z_PIECE):
-            self.rotations = Image.open('right_z.png').load()
+        elif (self.shape == self.RIGHT_J_PIECE):
+            im = Image.open('right_j.png')
             self.color = "Pink"
         else:
             print("Invalid piece type")
             return None
+
+        self.size = im.size[0]
+        self.rotations = im.load()
 
     def get_next_rotation(self):
         ret = self.rot_index + 1
@@ -66,43 +69,61 @@ class Piece:
 
         return ret
 
-    def point_out_of_bounds(self, test):
-        if test[0] > self.game_state.board_width - 1:
+    def point_out_of_bounds(self, x, y):
+        print("point oob")
+        print(x)
+        print(y)
+        if x > self.game_state.board_width - 1:
+            print("returning true")
             return True
-        elif test[0] < 0:
+        elif x < 0:
+            print("x less than 0")
             return True
-        elif test[1] > self.game_state.board_height - 1:
+        elif y > self.game_state.board_height - 1:
             return True
-        elif test[1] < 0:
+        elif y < 0:
             return True
         else:
             return False
 
     def rotation_unobstructed(self, test):
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if (self.rotations[i, (self.get_next_rotation() * 4) + j][0] == 0 and self.game_state.board[self.x + i + test[0]][self.y + j + test[1]] != None) or self.point_out_of_bounds(test):
-                    return False
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if self.rotations[i, (self.get_next_rotation() * self.size) + j][0] == 0:
+                        print(test)
+                        if self.point_out_of_bounds(self.x + i + test[0], self.y + j + test[1]):
+                            return False
+                        if self.game_state.board[self.x + i + test[0]][self.y + j + test[1]] != None:
+                            return False
 
         return True
 
     def rotate(self, width, height):
         can_rotate = False
-        for test in wallkick_offsets[self.get_next_rotation()]:
+        if self.shape == Piece.I_PIECE:
+            offsets = wallkick_offsets_i
+        else:
+            offsets = wallkick_offsets
+
+        for test in offsets[self.get_next_rotation()]:
             if self.rotation_unobstructed(test):
                 print("Rotation good")
                 can_rotate = True
+                self.x += test[0]
+                self.y += test[1]
                 break
             else:
                 print("Rotation bad")
 
         if can_rotate == True:
             self.rot_index += 1
+            if self.rot_index == 4:
+                self.rot_index = 0
 
     def draw(self):
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if self.rotations[i, (self.rot_index * 4) + j][0] == 0:
+        for i in range(0, self.size):
+            for j in range(0, self.size):
+                if self.rotations[i, (self.rot_index * self.size) + j][0] == 0:
                     screen.blit(block, (self.x * 16 + i * 16, self.y * 16 + j * 16))
 
 class Block:
@@ -113,7 +134,7 @@ class GameState:
     def __init__(self):
         self.running = True
         self.tick_count = 0
-        self.tick_mod = 50
+        self.tick_mod = 200
         self.current_piece = Piece(self)
         if (self.current_piece == None):
             print("Failed to create piece")
@@ -123,7 +144,7 @@ class GameState:
         self.board_width = 10
         self.board_height = 20
 
-        self.board = [[None for y in range(self.board_height)] for x in range(self.board_width)]
+        self.board = [[None for y in range(self.board_height + 1)] for x in range(self.board_width + 1)]
 
     def tick(self):
         self.tick_count += 1
@@ -142,11 +163,34 @@ class GameState:
 
         self.draw_screen()
 
+    def attempt_move_right(self):
+        for i in range(0, self.current_piece.size):
+            for j in range(0, self.current_piece.size):
+                if self.current_piece.rotations[i, (self.current_piece.get_next_rotation() * self.current_piece.size) + j][0] == 0:
+                    print("Current location")
+                    print(self.current_piece.x + i + 1)
+                    print(self.current_piece.y + j)
+                    if self.current_piece.point_out_of_bounds(self.current_piece.x + i + 1, self.current_piece.y + j):
+                        return False
+                    else:
+                        self.current_piece.x += 1
+                        return True
+
+    def attempt_move_left(self):
+        for i in range(0, self.current_piece.size):
+            for j in range(0, self.current_piece.size):
+                if self.current_piece.rotations[i, (self.current_piece.get_next_rotation() * self.current_piece.size) + j][0] == 0:
+                    if self.current_piece.point_out_of_bounds(self.current_piece.x + i - 1, self.current_piece.y + j):
+                        return False
+                    else:
+                        self.current_piece.x -= 1
+                        return True
+
     def handle_keydown(self, key):
         if key == pygame.K_RIGHT:
-            self.current_piece.x += 1
+            self.attempt_move_right()
         elif key == pygame.K_LEFT:
-            self.current_piece.x -= 1
+            self.attempt_move_left()
         elif key == pygame.K_UP:
             self.current_piece.rotate(self.board_width, self.board_height)
 
@@ -155,9 +199,9 @@ class GameState:
 
     # TODO: Clean this up
     def drop_piece(self):
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if self.current_piece.rotations[i, (self.current_piece.rot_index * 4) + j][0] == 0:
+        for i in range(0, self.current_piece.size):
+            for j in range(0, self.current_piece.size):
+                if self.current_piece.rotations[i, (self.current_piece.rot_index * self.current_piece.size) + j][0] == 0:
                     if self.current_piece.y + j + 1 > self.board_height - 1:
                         self.lock_piece()
                         return 1
@@ -170,9 +214,9 @@ class GameState:
 
     # TODO: Clean this up...
     def lock_piece(self):
-        for i in range(0, 4):
-            for j in range(0, 4):
-                if self.current_piece.rotations[i, (self.current_piece.rot_index * 4) + j][0] == 0:
+        for i in range(0, self.current_piece.size):
+            for j in range(0, self.current_piece.size):
+                if self.current_piece.rotations[i, (self.current_piece.rot_index * self.current_piece.size) + j][0] == 0:
                     self.board[self.current_piece.x + i][self.current_piece.y + j] = Block(self.current_piece.color)
                      
     def draw_board(self):
